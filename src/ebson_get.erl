ac3b0d1eeea8e9b327ec2a6330646d10c2b8728d
Jordan West <jordanrw@gmail.com>
2011-03-25 18:16:50 -0700

    check if bson binary contains a given key (returns 'true' or 'false')

--- src/ebson_get.erl
+++ src/ebson_get.erl
@@ -8,12 +8,22 @@
 %%%-------------------------------------------------------------------
 -module(ebson_get).
 
--export([value/2]).
+-export([value/2, has_key/2]).
 
-value(_Key, <<5, 0, 0, 0, 0>>) ->
+-define(EBSON_EMPTY_BINARY, <<5, 0, 0, 0, 0>>).
+
+value(_Key, ?EBSON_EMPTY_BINARY) ->
     undefined;
 value(Key, <<_Size:4/little-signed-integer-unit:8, Doc/binary>>) ->
     get_value(Key, Doc).
+
+has_key(_Key, ?EBSON_EMPTY_BINARY) ->
+    false;
+has_key(Key, <<Size:4/little-signed-integer-unit:8, Rest/binary>>) ->
+    DocSize = Size - 5,
+    <<Doc:DocSize/binary-unit:8, 0>> = Rest,
+    do_has_key(Key, Doc).
+
 
 get_value(Key, <<TypeFlag:1/binary-unit:8, AtKey/binary>>) ->
     FieldType = ebson_decode:field_type(TypeFlag),
@@ -25,7 +35,18 @@ get_value(Key, <<TypeFlag:1/binary-unit:8, AtKey/binary>>) ->
 	    AtNextFlag = skip_value(FieldType, AtValue),
 	    get_value(Key, AtNextFlag)
     end.
-	
+
+do_has_key(_Key, <<>>) ->
+    false;
+do_has_key(Key, <<TypeFlag:1/binary-unit:8, AtKey/binary>>) ->
+    FieldType = ebson_decode:field_type(TypeFlag),
+    case ebson_decode:key(AtKey) of
+	{Key, _} ->
+	    true;
+	{_, AtValue} ->
+	    AtNextFlag = skip_value(FieldType, AtValue),
+	    do_has_key(Key, AtNextFlag)
+    end.
 
 %% THERE IS A MUCH BETTER WAY TO DO THIS
 skip_value(float, <<_:8/little-float-unit:8, Rest/binary>>) ->
--- test/ebson_get_SUITE.erl
+++ test/ebson_get_SUITE.erl
@@ -13,12 +13,21 @@
 -export([all/0]).
 -export([value_from_empty_binary_doc/1, 
 	 value_from_first_key_in_binary_doc/1,
-	 value_from_not_first_key_in_binary_doc/1]).
+	 value_from_not_first_key_in_binary_doc/1,
+	 has_key_is_false_for_any_key_when_empty_bin_doc/1,
+	 has_key_is_true_for_first_key_in_bin_doc/1,
+	 has_key_is_true_for_any_existing_key_in_bin_doc/1,
+	 has_key_is_false_for_any_non_existing_key_in_bin_doc/1
+	]).    
 
 all() -> 
     [value_from_empty_binary_doc,
      value_from_first_key_in_binary_doc,
-     value_from_not_first_key_in_binary_doc].
+     value_from_not_first_key_in_binary_doc,
+     has_key_is_false_for_any_key_when_empty_bin_doc,
+     has_key_is_true_for_first_key_in_bin_doc,
+     has_key_is_true_for_any_existing_key_in_bin_doc,
+     has_key_is_false_for_any_non_existing_key_in_bin_doc].
 
 value_from_empty_binary_doc(_) ->
     Doc = <<5, 0, 0, 0, 0>>,
@@ -36,3 +45,25 @@ value_from_not_first_key_in_binary_doc(_) ->
     Val = 3,
     Doc = ebson:encode([{<<"a">>, 1}, {<<"b">>, 2}, {Key, Val}, {<<"d">>, 4}]),
     Val = ebson_get:value(Key, Doc).
+
+has_key_is_false_for_any_key_when_empty_bin_doc(_) ->
+    Key = <<"whocares">>,
+    Doc = ebson:encode([]),
+    false = ebson_get:has_key(Key, Doc).
+
+has_key_is_true_for_first_key_in_bin_doc(_) ->
+    Key = <<"abc">>,
+    Doc = ebson:encode([{Key, 1}]),
+    true = ebson_get:has_key(Key, Doc).
+
+has_key_is_true_for_any_existing_key_in_bin_doc(_) ->
+    Key = <<"c">>,
+    Val = 3,
+    Doc = ebson:encode([{<<"a">>, 1}, {<<"b">>, 2}, {Key, Val}, {<<"d">>, 4}]),
+    true = ebson_get:has_key(Key, Doc).
+
+has_key_is_false_for_any_non_existing_key_in_bin_doc(_) ->
+    Key = <<"e">>,
+    Doc = ebson:encode([{<<"a">>, 1}, {<<"b">>, 2}, {<<"c">>, 3}, {<<"d">>, 4}]),
+    false = ebson_get:has_key(Key, Doc).
+    
