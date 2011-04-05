995bbf55b66f6dfff9de1ba0fbc14f4bc6be03f2
Jordan West <jordanrw@gmail.com>
2011-04-04 21:22:14 -0700

    added has_key/2 to ebson module

--- src/ebson.erl
+++ src/ebson.erl
@@ -8,7 +8,7 @@
 %%%-------------------------------------------------------------------
 -module(ebson).
 
--export([encode/1, decode/1, doc_size/1]).
+-export([encode/1, decode/1, doc_size/1, has_key/2]).
 -include("../include/ebson.hrl").
 
 -spec encode(ebson_pl_doc()) -> binary().		     
@@ -22,3 +22,7 @@ decode(Bin) when is_binary(Bin) ->
 -spec doc_size(binary()) -> integer().
 doc_size(Bin) ->
     ebson_get:doc_size(Bin).
+
+-spec has_key(binary(), binary()) -> boolean().
+has_key(Key, Bin) when is_binary(Key) andalso is_binary(Bin) ->
+    ebson_get:has_key(Key, Bin).
